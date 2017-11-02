943c2976990c8e63db19134dd3a36504884c93af
Lewis Hopkins <lewis.j.hopkins@gmail.com>
2017-11-02 21:38:56 +1000

    remove some bits and pieces from the config, they were just test values anyway, and non-sensitive

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     reactionDbFile: "./database/db",
     summaryLogDbFile: "./database/summaries",
-    slackToken: "xoxb-210870825027-eNdCda96yEZEqFy0DVWUG8oS",
-    slackUrl: "https://q-mmittest.slack.com",
+    slackToken: "",
+    slackUrl: "",
     trackedReactions: ["+1","taco"],
     summarySchedule: "every 1 min",//"on friday at 3:30pm",//text expression as per https://bunkat.github.io/later/parsers.html#text
-    summaryOutputChannel: "C0B0U1EE6",
+    summaryOutputChannel: "",
     summaryIntroText: "Awards for this week",
     summaryNoAwardsText: "Couldn't find any awards for this week. Either I'm doing something wrong or you are",
     devMode: true,
